c53348a47ad5900d7384b368ef77cd8b41c56128
YOKOTATE-PC\yokotate <yokosekazuya@yahoo.co.jp>
2018-09-22 20:45:33 +0900

    データを取得してそれをデータベースに保存するコード

--- code/CoinCheckGetValue.py
+++ code/CoinCheckGetValue.py
@@ -2,35 +2,30 @@
 
 import requests
 import psycopg2
+import DataAccess as da
+
 class CoinCheckGetCValue():
     # coincheck で取得
-    res = requests.get('https://coincheck.com/api/ticker')
-    jsonData = res.json()
-    # last 直近売買
-    # bid 買値
-    # ask 売値
-    # high  24時間での最高取引価格
-    # low  24時間での最安取引価格
-    # volume 24時間での取引量 
-    connection = psycopg2.connect("host=localhost port=5432 dbname=test user=postgres password=k_yokose1330076")
-    # connection = psycopg2.connect("host=localhost port=5432 dbname=DBNAME user=USER password=PASSWORD")
-    connection.get_backend_pid()
-
-    cur = connection.cursor()
-
-    last = jsonData["last"]
-    bid  = jsonData["bid"]
-    ask  = jsonData["ask"]
-    high  = jsonData["high"]
-    low  = jsonData["low"]
-    volume  = jsonData["volume"]
-    
-    sql  = "INSERT INTO BTC_VALUE (last_val,bid_val,ask_val,high_val,low_val,volume_val)"
-    sql += " VALUES(" + str(last) + "," + str(bid) + "," + str(ask) +  "," + str(high) + "," + str(low) + "," + str(volume) + ")"
-
-    cur.execute(sql)
-    connection.commit()
-    # print(jsonData)
+    def InsBTC_VALUE(self):
+        res = requests.get('https://coincheck.com/api/ticker')
+        jsonData = res.json()
+        # last 直近売買
+        # bid 買値
+        # ask 売値
+        # high  24時間での最高取引価格
+        # low  24時間での最安取引価格
+        # volume 24時間での取引量 
+        last = jsonData["last"]
+        bid  = jsonData["bid"]
+        ask  = jsonData["ask"]
+        high  = jsonData["high"]
+        low  = jsonData["low"]
+        volume  = jsonData["volume"]
+        
+        # 取得したデータを保存用プログラムに投げる
+        env = da.DataAccess()
+        env.InsertToBTC_VALUE(last,bid,ask,high,low,volume)
 
 if __name__ == "__main__":
     env = CoinCheckGetCValue()
+    env.InsBTC_VALUE()
--- code/DataAccess.py
+++ code/DataAccess.py
@@ -2,30 +2,30 @@
 import psycopg2
 
 class DataAccess():
-    # init
-    # Connection info
+    # 初期値設定
+    # ここの設定は自分の環境に合わせて書き換える
     def __init__(self):
         self.path = "localhost"
         self.port = "5432"
-        self.dbname = "dbname"
+        self.dbname = "test"
         self.user = "postgres"
-        self.password = "pass"
+        self.password = "password"
 
     # データベース接続
+    # プライベート関数
     # カーソルとコネクションを返します。
-    def ConectToDB(self):
+    def __ConectToDB(self):
         conText = "host={} port={} dbname={} user={} password={}"
         conText = conText.format(self.path,self.port,self.dbname,self.user,self.password)
         connection = psycopg2.connect(conText)
         connection.get_backend_pid()
-        print(connection)
         cur = connection.cursor()
         return cur,connection
 
     # BTC_VALUEテーブルに対するInsertを実行します。
     # 6つのデータが必要です。
     def InsertToBTC_VALUE(self,last,bid,ask,high,low,volume):
-        cur,connection = self.ConectToDB()
+        cur,connection = self.__ConectToDB()
         sql  = "INSERT INTO BTC_VALUE (last_val,bid_val,ask_val,high_val,low_val,volume_val) VALUES({},{},{},{},{},{})"
         sql  = sql.format(last,bid,ask,high,low,volume)
         cur.execute(sql)
@@ -33,11 +33,22 @@ class DataAccess():
         cur.close()
         connection.close()
     
+    # BTC_VALUEテーブルからデータを全て取得し、List形で返す
     def SelectBTC_VALUE(self):
-        cur,connection = self.ConectToDB()
-        sql = ""
+        BtcList = []
+        cur,connection = self.__ConectToDB()
+        sql = "SELECT * FROM BTC_VALUE"
+        cur.execute(sql)
+        # 取得したデータをList形に変換
+        for row in cur:
+            BtcList.append(row)
+        cur.close()
+        connection.close()
+        return BtcList
         
-
+        
 if __name__ == "__main__":
     env = DataAccess()
-    env.InsertToBTC_VALUE
+    env.SelectBTC_VALUE()
+
+    
