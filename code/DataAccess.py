823dfb509cc9df7f943d0006d762bb4e9bbdf04a
YOKOTATE-PC\yokotate <yokosekazuya@yahoo.co.jp>
2018-09-22 12:30:44 +0900

    dbnameを忘れてたので追加

--- code/DataAccess.py
+++ code/DataAccess.py
@@ -5,32 +5,31 @@ class DataAccess():
     # init
     # Connection info
     def __init__(self):
-        self.path = "path"
+        self.path = "localhost"
         self.port = "5432"
-        self.user = "user"
+        self.dbname = "dbname"
+        self.user = "postgres"
         self.password = "pass"
 
-    # Connect to databasae
-    # return cursor and connection
+    # データベース接続
+    # カーソルとコネクションを返します。
     def ConectToDB(self):
         conText = "host={} port={} dbname={} user={} password={}"
-        conText = conText.format(self.path,self.port,self.user,self.password)
+        conText = conText.format(self.path,self.port,self.dbname,self.user,self.password)
         connection = psycopg2.connect(conText)
         connection.get_backend_pid()
         print(connection)
         cur = connection.cursor()
         return cur,connection
 
+    # BTC_VALUEテーブルに対するInsertを実行します。
+    # 6つのデータが必要です。
     def InsertToBTC_VALUE(self,last,bid,ask,high,low,volume):
-
         cur,connection = self.ConectToDB()
-
         sql  = "INSERT INTO BTC_VALUE (last_val,bid_val,ask_val,high_val,low_val,volume_val) VALUES({},{},{},{},{},{})"
         sql  = sql.format(last,bid,ask,high,low,volume)
-
         cur.execute(sql)
         connection.commit()
-
         cur.close()
         connection.close()
     
