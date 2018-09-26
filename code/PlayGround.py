68fe0aea79504bffc223ce93e6b89cc3fb2cb297
YOKOTATE-PC\yokotate <yokosekazuya@yahoo.co.jp>
2018-09-26 23:06:24 +0900

    色々必要なものを追加 ・ログ記録を追加 ・24時間平均と1時間平均の追加 ・必要情報のリスト化 ・こまごまとした修正

--- code/PlayGround.py
+++ code/PlayGround.py
@@ -35,13 +35,17 @@ class PlayGround():
         # テスト用（削除してね！！）
 
     # 各種仮想通貨データを取得
-    def DataSet(self,last,bid,ask,high,low,volume):
-        self.last   = last
-        self.bid    = bid
-        self.ask    = ask
-        self.high   = high
-        self.low    = low
-        self.volume = volume
+    def DataSet(self,last,bid,ask,high,low,volume,hour24_bid,hour24_ask,hour_bid,hour_ask):
+        self.last       = last
+        self.bid        = bid
+        self.ask        = ask
+        self.high       = high
+        self.low        = low
+        self.volume     = volume
+        self.hour24_bid = hour24_bid
+        self.hour24_ask = hour24_ask
+        self.hour_bid   = hour_bid
+        self.hour_ask   = hour_ask
     
     # 取れる行動をリスト形式で返す
     def EnableActionList(self):
@@ -52,15 +56,37 @@ class PlayGround():
         # ビットコインを持っていなければ購入ができる
         else:
             actlist.append(1)
+        return actlist
+    
+    # 必要情報をリスト形式で返す
+    def Returninfo(self):
+        if self.BitcoinFlag == True:
+            Flag = 1
+        else:
+            Flag = 0
+        # 0から1の間にまとめるための正規化
+        l = np.array([self.last,self.bid,self.hour24_bid,self.hour_bid,self.ask,self.hour24_ask,self.hour_ask,self.high,self.low,self.volume,self.MyMoney,self.MyCoin,self.MyCoinGetValue])
+        l = l.astype(int)
+        lmax = l.max()
+        lmin = l.min()
+        average = (l - lmin).astype(float) / (lmax - lmin).astype(float)
+        average = np.append(average, np.array([Flag]))
+        infolist = []
+        infolist.append(average)
+        return infolist
     
     # 行動の実行
     def action(self,action):
         if action == 1:
             self.BuyAction()
+            self.reset()
+            return 0
         elif action == 2:
+            self.reset()
             return self.SellAction()
         else:
             self.NoAction()
+            return self.MyMoney
 
     # 何もしないメソッド
     def NoAction(self):
@@ -68,15 +94,23 @@ class PlayGround():
 
     # 購入するメソッド
     def BuyAction(self):
-        BuyVolume = self.MyMoney / self.bid
+        BuyVolume = self.MyMoney / self.ask
         self.env.UpdateUserDataBuy(BuyVolume, self.bid)
+        self.reset()
 
     # 売却するメソッド
     def SellAction(self):
-        sellmoney = self.ask * self.MyCoin
+        sellmoney = self.bid * self.MyCoin
         self.env.UpdateUserDataSell(sellmoney)
+        self.reset()
         return sellmoney
-
+    
+    def ReturnResult(self):
+        if self.MyMoney > 0:
+            return self.MyMoney
+        else:
+            return self.bid * self.MyCoin
+    
 if __name__ == "__main__":
     env = PlayGround()
     env.BuyAction()
