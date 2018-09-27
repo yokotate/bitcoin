8e0a0f2054a8b36c40a47dc10b9790d6c161639d
Administrator <admin@example.com>
2018-09-27 12:59:42 +0900

    ラベルの追加 コメントの追加

--- code/BtcPlot.py
+++ code/BtcPlot.py
@@ -3,13 +3,17 @@ import numpy as np
 import sys
 import matplotlib.pyplot as plt
 
+# 平均集計用変数
 hour24_bid = np.array([])
 hour24_ask = np.array([])
 hour_bid   = np.array([])
 hour_ask   = np.array([])
 
+# DBアクセス
 daenv = da.DataAccess()
 data = daenv.SelectBTC_VALUE()
+
+# データ収集用
 i=0
 l24ask = []
 ask = []
@@ -20,11 +24,13 @@ lbid   = []
 lh =[]
 ll = []
 li = []
-# データ個数分トレーニングを実行する
+
+# データ収集開始
 for row in data:
     i = i + 1
     if i <= 1439:
         continue
+    # 平均値算出用データ集計
     hour24_bid = np.array(data[i - 1440:i])[:,2]
     hour24_ask = np.array(data[i - 1440:i])[:,3]
     hour_bid   = np.array(data[i - 60:i])[:,2]
@@ -41,15 +47,25 @@ for row in data:
     ll.append(row[5])
     li.append(i -1440)
 
+
+# 描画設定
 fig = plt.figure()
 ax = fig.add_subplot(1,1,1)
-
-ax.plot(li,l24bid)
-ax.plot(li,l24ask)
-ax.plot(li,bid)
-ax.plot(li,ask)
-ax.plot(li,lask)
-ax.plot(li,lbid)
-ax.plot(li,lh)
-ax.plot(li,ll)
+# 描画データ設定
+ax.plot(li, l24bid, label = "買値（24時間平均）")
+ax.plot(li, lbid,   label = "買値（1時間平均）")
+ax.plot(li, bid,    label = "買値（現在）")
+ax.plot(li, l24ask, label = "売値（24時間平均）")
+ax.plot(li, lask,   label = "売値（1時間平均）")
+ax.plot(li, ask,    label = "売値（現在）")
+ax.plot(li, lh,     label = "最高売値（過去24時間）")
+ax.plot(li, ll,     label = "最低買値（過去24時間）")
+# ラベル表示
+plt.legend()
+# タイトル
+plt.title("ビットコイン価格推移")
+# 軸ラベル表示
+plt.xlabel("時間")
+plt.ylabel("価格")
+# 描画
 plt.show()
